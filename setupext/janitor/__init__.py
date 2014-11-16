c071708f0378a853b0bcf3aebce3555d5539edf6
Dave Shawley <daveshawley@gmail.com>
2014-11-16 15:06:23 -0500

    Update for 0.0.1 release.

--- setupext/janitor/__init__.py
+++ setupext/janitor/__init__.py
@@ -3,7 +3,7 @@ from distutils.command.clean import clean as _CleanCommand
 import shutil
 
 
-version_info = (0, 0, 0)
+version_info = (0, 0, 1)
 __version__ = '.'.join(str(v) for v in version_info)
 
 
