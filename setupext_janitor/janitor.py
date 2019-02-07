b72552945203e76066e28f35631982a57b67c1c2
matt wilkie <maphew@gmail.com>
2019-02-07 12:30:20 -0800

    WIP: don't exit on error, but we don't deal with it either!

--- setupext_janitor/janitor.py
+++ setupext_janitor/janitor.py
@@ -70,17 +70,19 @@ class CleanCommand(_CleanCommand):
         if self.dist:
             for cmd_name, _ in self.distribution.get_command_list():
                 if 'dist' in cmd_name:
-                    #print('--- start of if dist in cmd_name --') #mhw
                     command = self.distribution.get_command_obj(cmd_name)
-                    #print(command)
+                    
+                    # mhw debug: this stops premature exit for RPM-on-NT err
+                    # but means real fail will skipped too. Probably needs
+                    # a better fix.
+                    #  
                     try:
                         command.ensure_finalized()
                     except Exception as e:
                         print(f'\n*** Exception encountered and ignored:\n\t{command}\n\t{e}\n')
-                    #print('passed command.ensure_finalized()') #mhw
+
                     if getattr(command, 'dist_dir', None):
                         dir_names.add(command.dist_dir)
-                    #print('--- end of if dist in cmd_name --') #mhw
                     
         if self.eggs:
             for name in os.listdir(self.egg_base):
