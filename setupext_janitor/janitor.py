3dca206fc474aa8268fcdac4538c3e4ea68837a2
matt wilkie <maphew@gmail.com>
2019-02-07 11:34:10 -0800

    WIP: identify where RPM exit occurs (#12)

--- setupext_janitor/janitor.py
+++ setupext_janitor/janitor.py
@@ -70,11 +70,15 @@ class CleanCommand(_CleanCommand):
         if self.dist:
             for cmd_name, _ in self.distribution.get_command_list():
                 if 'dist' in cmd_name:
+                    print('--- start of if dist in cmd_name --') #mhw
                     command = self.distribution.get_command_obj(cmd_name)
+                    print(command)
                     command.ensure_finalized()
+                    print('passed command.ensure_finalized()') #mhw
                     if getattr(command, 'dist_dir', None):
                         dir_names.add(command.dist_dir)
-
+                    print('--- end of if dist in cmd_name --') #mhw
+                    
         if self.eggs:
             for name in os.listdir(self.egg_base):
                 if name.endswith('.egg-info'):
