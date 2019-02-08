fba9e733035a81dcd6a59f168da60f9e12fb5670
matt wilkie <maphew@gmail.com>
2019-02-07 20:22:58 -0800

    increment version so can require in setup_requires
    using something lke:
    # https://github.com/pytest-dev/pytest-xdist/issues/136
    # https://github.com/pypa/pip/issues/5566

--- setupext_janitor/janitor.py
+++ setupext_janitor/janitor.py
@@ -3,7 +3,7 @@ from distutils.command.clean import clean as _CleanCommand
 import os.path
 import traceback
 
-version_info = (1, 0, 0)
+version_info = (1, 0, 1)
 __version__ = '.'.join(str(v) for v in version_info)
 debug = False
 
